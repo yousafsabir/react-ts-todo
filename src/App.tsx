084fe5cb7c834e2ae022fc01c5c150c0844e7f15
yousafsabir <yousafsabir430516@gmail.com>
2022-06-26 01:20:42 +0500

    All done except for the weird expansion of cards

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,33 @@ const App: React.FC = () => {
             console.log(todos);
         }
     };
+    const handleDel = (id: number): void => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    };
+    const handleEdit = (id: number, todo: string): void => {
+        setTodos(
+            todos.map((item) => {
+                if (item.id === id) {
+                    return { ...item, todo };
+                } else {
+                    return item;
+                }
+            })
+        );
+    };
+    const handleDone = (id: number) => {
+        setTodos(
+            todos.map((item) => {
+                if (item.id === id) {
+                    console.log("inside");
+
+                    return { ...item, isDone: true };
+                } else {
+                    return item;
+                }
+            })
+        );
+    };
     return (
         <div className="App">
             <header className="App-header">
@@ -25,9 +52,17 @@ const App: React.FC = () => {
             </header>
             <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
             <div className="card-grid">
-            {todos.map((todo) => {
-                return <TodoWrap key={todo.id} {...todo} />;
-            })}
+                {todos.map((todo) => {
+                    return (
+                        <TodoWrap
+                            key={todo.id}
+                            {...todo}
+                            delIt={handleDel}
+                            editIt={handleEdit}
+                            doneIt={handleDone}
+                        />
+                    );
+                })}
             </div>
         </div>
     );
--- src/components/inputField/TodoWrap.tsx
+++ src/components/inputField/TodoWrap.tsx
@@ -1,23 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiEditAlt, BiTrash } from "react-icons/bi";
 import { MdDone } from "react-icons/md";
+import { FcCancel } from "react-icons/fc";
 import "./TodoWrap.css";
 
 type Props = {
     id: number;
     todo: string;
     isDone: boolean;
+    delIt: (id: number) => void;
+    doneIt: (id: number) => void;
+    editIt: (id: number, todo: string) => void;
 };
 
-const TodoWrap: React.FC<Props> = ({ id, todo, isDone }) => {
+const TodoWrap: React.FC<Props> = ({
+    id,
+    todo,
+    isDone,
+    delIt,
+    editIt,
+    doneIt,
+}) => {
+    const [edit, setEdit] = useState<boolean>(false);
+    const [editTodo, setEditTodo] = useState<string>("");
+
+    const styles = {
+        containerEdit: {
+            display: "flex",
+        },
+        containerTodo: {
+            maxWidth: "100%",
+            display: "flex",
+            gap: "0.25rem",
+            justifyContent: "space-between",
+        },
+        input: {
+            display: "inline-block",
+            flex: 1,
+        },
+        completed: {
+            textDecoration: isDone ? "line-through" : "none",
+            textDecorationColor: "red",
+        },
+    };
+
     return (
-        <div className="todo-wrap">
-            <div>{todo}</div>
-            <div className="right">
-                <BiEditAlt />
-                <BiTrash />
-                <MdDone />
-            </div>
+        <div
+            className="todo-wrap"
+            style={edit ? styles.containerEdit : styles.containerTodo}
+        >
+            {edit ? (
+                <input
+                    type="text"
+                    className="wrap-input"
+                    style={styles.input}
+                    value={editTodo}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        setEditTodo(e.target.value)
+                    }
+                />
+            ) : (
+                <p className="todo" style={styles.completed}>
+                    {todo}
+                </p>
+            )}
+            {edit ? (
+                <div className="right">
+                    <MdDone
+                        onClick={() => {
+                            editIt(id, editTodo);
+                            setEdit(false);
+                        }}
+                    />
+                    <FcCancel onClick={() => setEdit(false)} />
+                </div>
+            ) : (
+                <div className="right">
+                    <BiEditAlt
+                        onClick={() => {
+                            setEdit(true);
+                            setEditTodo(todo);
+                        }}
+                    />
+                    <BiTrash onClick={() => delIt(id)} />
+                    <MdDone
+                        onClick={() => {
+                            if (!isDone) {
+                                doneIt(id);
+                            }
+                        }}
+                    />
+                </div>
+            )}
         </div>
     );
 };
